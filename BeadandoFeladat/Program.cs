9ad9b8cfef1c7be2837e4d291e219e2d6e21ec93
Lyen <julien.apatini@gmail.com>
2019-09-20 12:43:50 +0200

    Negyedik feladat

--- BeadandoFeladat/Program.cs
+++ BeadandoFeladat/Program.cs
@@ -10,48 +10,54 @@ namespace BeadandoFeladat
     {
         static void Harmadik()
         {
+            try
+            {
+                Console.WriteLine("Adja meg, hány számot kérjünk be!");
+                int N = Convert.ToInt32(Console.ReadLine());
+                int[] A = new int[N];
+                Console.WriteLine("Írjon be {0} számot!", N);
+                int bekert;
+
+                for (int i = 0; i < A.Length; i++)
+                {
+                    bekert = Convert.ToInt32(Console.ReadLine());
+                    A[i] = bekert;
+                }
+                Console.WriteLine();
+                for (int i = 0; i < A.Length; i++)
+                {
+                    Console.Write(A[i] + " ");
+                }
+                Console.WriteLine();
+                int max = A[0];
+                for (int i = 0; i < A.Length; i++)
+                {
+                    if (max < A[i])
+                    {
+                        max = A[i];
+                    }
+                }
+                int min = A[0];
+                for (int i = 0; i < A.Length; i++)
+                {
+                    if (min > A[i])
+                    {
+                        min = A[i];
+                    }
+                }
+                Console.WriteLine("A legnagyobb szám: {0}, a legkisebb: {1}", max, min);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Hiba, a program leáll.");
+            }
             
-            Console.WriteLine("Adja meg, hány számot kérjünk be!");
-            int N= Convert.ToInt32(Console.ReadLine());
-            int[] A = new int[N];
-            Console.WriteLine("Írjon be {0} számot!",N);
-            int bekert;
-
-            for (int i = 0; i < A.Length; i++)
-            {
-                bekert = Convert.ToInt32(Console.ReadLine());
-                A[i] = bekert;
-            }
-            Console.WriteLine();
-            for (int i = 0; i < A.Length; i++)
-            {
-                Console.Write(A[i] + " ");
-            }
-            Console.WriteLine();
-            int max = A[0];
-            for (int i = 0; i < A.Length; i++)
-            {
-                if (max<A[i])
-                {
-                    max = A[i];
-                }
-            }
-            int min = A[0];
-            for (int i = 0; i < A.Length; i++)
-            {
-                if (min > A[i])
-                {
-                    min = A[i];
-                }
-            }
-            Console.WriteLine("A legnagyobb szám: {0}, a legkisebb: {1}",max,min);
-
+            
 
         }
         static void Main(string[] args)
         {
             Harmadik();
-
 
             Console.ReadLine();
         }
