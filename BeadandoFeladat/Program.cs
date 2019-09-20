0214997660dab4a0c8204794ad3c9ca02fc66c03
Lyen <julien.apatini@gmail.com>
2019-09-20 12:19:44 +0200

    ElsoFeladat

--- BeadandoFeladat/Program.cs
+++ BeadandoFeladat/Program.cs
@@ -8,9 +8,43 @@ namespace BeadandoFeladat
 {
     class Program
     {
+        static void Elso()
+        {
+            int[] A = new int[10];
+
+            Console.WriteLine("Írjon be 10 számot!");
+
+            int bekert;
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                bekert = Convert.ToInt32(Console.ReadLine());
+                A[i] = bekert;
+            }
+            Console.WriteLine();
+            for (int i = 0; i < A.Length; i++)
+            {
+                Console.Write(A[i] + " ");
+            }
+            Console.WriteLine();
+            int max = A[0];
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (max<A[i])
+                {
+                    max = A[i];
+                }
+            }
+            Console.WriteLine("A legnagyobb szám: {0}",max);
+
+
+        }
         static void Main(string[] args)
         {
+            Elso();
 
+
+            Console.ReadLine();
         }
     }
 }
